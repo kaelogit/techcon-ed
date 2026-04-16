6122629e10c0f03ca2af7c747d857340d92f2077
kaelogit <ksqkareem@gmail.com>
2026-04-17 00:37:17 +0100

    ok

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -120,7 +120,7 @@ export function Navbar() {
 
       {/* Mobile Menu Overlay */}
       <div 
-        className={`fixed inset-0 bg-(--warm-cream) z-40 flex flex-col transition-all duration-500 ease-out lg:hidden ${
+        className={`fixed inset-0 bg-white z-40 flex flex-col transition-all duration-500 ease-out lg:hidden ${
           isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
         }`}
       >
